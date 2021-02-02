87c32ae47fb5b8bfe373367e331badd6efd1bb45
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-02-02 12:08:05 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// const getDataJokes = async () => {
+//     const res = await axios.get('https://icanhazdadjoke.com', {
+//         headers: { 'Accept': 'application/json' }
+//     });
+//     // console.log(res.data);
+//     showJokes(res.data);
+// };
+
+// getDataJokes();
+
+const getDataJokes = async () => {
+    const res = await axios.get('https://icanhazdadjoke.com', {
+        headers: { 'Accept': 'application/json' }
+    });
+    console.log(res.data);
+    const main = document.querySelector('ul');
+    const btn = document.querySelector('button');
+    btn.addEventListener('click', (e) => {
+        getDataJokes().then(()=>{
+            const list = document.createElement('li');
+            list.innerHTML = `<span>${res.data.joke}</span>`;
+            main.appendChild(list);
+        })        
+    });
+};
+
+getDataJokes();
+
+
+
